8c00b85e12f5cb765a1a673b67ba0fd0d07893b8
marbokbgd-cmyk <marbok.bgd@gmail.com>
2026-09-03 16:19:35 +0200

    Enable admin offer selection across pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,21 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 import "./globals.css";
 import { StoreProvider } from "@/context/StoreContext";
+import { OfferSelectionProvider } from "@/context/OfferSelectionContext";
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <StoreProvider>
-            <QueryClientProvider client={queryClient}>
-                <Hydrate state={pageProps.dehydratedState}>
-                    <div id="modal" className="modal"></div>
-                    <Component {...pageProps} />
-                </Hydrate>
-            </QueryClientProvider>
+            <OfferSelectionProvider>
+                <QueryClientProvider client={queryClient}>
+                    <Hydrate state={pageProps.dehydratedState}>
+                        <div id="modal" className="modal"></div>
+                        <Component {...pageProps} />
+                    </Hydrate>
+                </QueryClientProvider>
+            </OfferSelectionProvider>
         </StoreProvider>
     );
 }
